c8c941d79b3c7021d4147e131e180aac9d24c46a
Renan5953 <172679455+Renan5953@users.noreply.github.com>
2025-12-09 15:39:49 -0300

    Basic logic for etch-a-sketch
    - Create default 16x16 grid on page load.
    - Add button to generate a custom-sized grid.
    - Add button to clear the current grid.
    - Add initial CSS styling for the container and blocks.
    - Add hover logic to color the blocks on mouseover.

---
+++ script.js
@@ -0,0 +1,46 @@
+const proportion = document.querySelector(".proportion");
+const clearBtn = document.querySelector(".clear");
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 256; i++) {
+    const block = document.createElement("div");
+    container.appendChild(block);
+}
+
+container.addEventListener('mouseover', (e) => {
+    const target = e.target;
+
+    if (target.className !== "container") {
+        target.classList = "hovered";
+    }
+});
+
+proportion.addEventListener('click', () => {
+    container.replaceChildren();
+
+    let value = +prompt("Set the proportion of the grid:");
+
+    while ((!Number.isInteger(value)) || value > 100 || value <= 0) {
+        alert("Please, set a positive, integer number equal or smaller than 100.");
+        value = +prompt("Set the proportion of the grid:");
+    }
+
+    for (let i = 0; i < (value ** 2); i++) {
+        const block = document.createElement("div");
+        container.appendChild(block);
+    }
+
+    const blocks = document.querySelectorAll(".container div");
+    
+    blocks.forEach(item => {
+        item.style.width = `calc(100% / ${value})`;
+    });
+});
+
+clearBtn.addEventListener('click', () => {
+    const blocks = document.querySelectorAll(".container div");
+
+    blocks.forEach(item => {
+        item.classList = "";
+    });
+});
